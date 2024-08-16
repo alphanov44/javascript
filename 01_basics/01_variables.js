0bd8b612772d27fa5b7ce4309eac857b71af49c9
alphanov44 <159747338+alphanov44@users.noreply.github.com>
2024-08-16 19:07:29 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const contactId=12345566
+let accountEmail="aditi@gmail.com"
+var accountPassword="sabkhush"
+accountCity="Jaipur"
+
+console.log(contactId);
+
+//contactId=2 //not allowed
+accountEmail="sona@gmail.com";
+accountPassword="aurkhush"
+accountCity="hyderabad"
+let myName
+
+//another method
+console.table([contactId,accountEmail,accountPassword,accountCity,myName]);
+//prefered not to use var 
+
+//when uninitialised there value is = undefined
