3ac4c1a7a271e1ce64136ca9e4549f28cb215ace
alphanov44 <159747338+alphanov44@users.noreply.github.com>
2024-08-16 19:26:40 +0000

    done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+let name="Aditi"
+let age=20
+let isloggedin=false
+
+//number=> 2 to power 53
+//bigint
+//string=" "
+
+//null = standalone value
+//undefined 
+//symbol = unique
+
+console.log(typeof age);
+
+console.log(typeof null) //object
+console.log(typeof undefined)//undefined
