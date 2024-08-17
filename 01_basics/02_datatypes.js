27d78338fcb3a3fa241eb7e56445e033b3679527
alphanov44 <159747338+alphanov44@users.noreply.github.com>
2024-08-17 05:15:55 +0000

    done

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -14,3 +14,20 @@ console.log(typeof age);
 
 console.log(typeof null) //object
 console.log(typeof undefined)//undefined
+
+/*
+Primitive Type
+7 Types: string,Number,Boolean,null,undefined,Symbol,BigInt
+Reference Type (Non primitive)
+eg : arrays,objects,funstions
+const heros=["sakti","naag","jaat"]
+let myobj={
+name:"Aditi"
+age: 20
+   }
+*/
+const myfunction=function()
+{
+    console.log("Hello world");
+}
+console.log(myfunction)
