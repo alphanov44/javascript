f586213122e37d4465a062c7c748299e72ccaca1
alphanov44 <159747338+alphanov44@users.noreply.github.com>
2024-08-17 06:49:24 +0000

    ..

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,27 @@
+const name="Aditi "
+const repocnt=40
+
+//console.log(name+repocnt+"value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repocnt}`); 
+
+const gameName=new String ('Aditi')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+//console.log(gameName.charAt(2));
+//const newString = gameName.substring(0,3)
+//console.log(newString);
+//slice 
+
+const newStringone="  aditi  ";
+console.log(newStringone);
+console.log(newStringone.trim());
+
+
+
+
