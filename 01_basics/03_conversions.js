d09b71459001f1cd487dbc04aa26eb965e67083b
alphanov44 <159747338+alphanov44@users.noreply.github.com>
2024-08-17 04:42:40 +0000

    done

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -19,7 +19,43 @@ let numvalue=Number(num)
 
 let isloggedin=1
 let boolans=Boolean(isloggedin)
-console.log(boolans);
+//console.log(boolans);
 //1=>true  0=>false
 //""=>false
 //"aditi"=>true
+
+//------------OPERATIONS!!------------------------------//
+
+let numval=3
+let negval=-numval
+//console.log(negval);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2%3)
+// console.log(2**3)
+
+let str1="Hello "
+let str2="Adtss!!"
+let str3=str1+str2
+//console.log(str3)
+
+// console.log("1" + 2); //12
+// console.log(1+"2");  //12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2"); //32
+
+// console.log("2">1) //true
+// console.log("02">1)//true
+
+// console.log(null>0); //false
+// console.log(null==0); //false
+// console.log(null>=0); //true
+
+//=== ie strict check
+console.log("2"==2); //true
+console.log("2"===2); //false
+
+
