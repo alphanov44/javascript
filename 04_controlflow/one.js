74a42641aa5e072d1d79f14a1f763831fa593857
alphanov44 <159747338+alphanov44@users.noreply.github.com>
2024-08-20 17:44:37 +0000

    ...

---
+++ 04_controlflow/one.js
@@ -0,0 +1,45 @@
+// >, <, >=, <=, !=, ==, ===, !==
+//if else ,,else if wale chiije
+
+//switch
+// switch(key)
+// {
+//     case Value:
+//         break;
+//     default:
+//         break;
+// }
+
+//FALSY VALUES:
+/*
+    false
+    0
+    -0
+    BigInt 0n
+    ""
+    null
+    undefined
+    NaN
+*/
+
+//truthy
+/*
+    "0",'false'," ",{},[],function(){}
+*/
+
+
+//Nullish coalescing Operator (??): null defined
+let val1;
+val1 = 5 ?? 10  //5
+val1 = null ?? 10 //10
+
+console.log(val1);
+
+//Terniary operator
+//condition ? true : false
+
+
+
+
+
+
