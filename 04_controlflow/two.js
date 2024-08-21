77e70442c3e0ec811d1d8e40d69b6cb9855bd9d6
alphanov44 <159747338+alphanov44@users.noreply.github.com>
2024-08-21 14:25:24 +0000

    .

---
+++ 04_controlflow/two.js
@@ -0,0 +1,33 @@
+//for of loop
+const arr=[1,2,3,4,5]
+
+for(const num of arr)
+{
+    //console.log(num);
+    
+}
+const greetings= "Hello world!"
+for(const greet of greetings)
+{
+    //console.log(`Each char is ${greet}`)
+} 
+
+const map=new Map()
+map.set('IN',"India")
+map.set('USA',"United states of America")
+map.set('UK',"United Kingdoms")
+map.set('Fr',"France")
+
+//console.log(map);
+
+for(const [key,value] of map)
+{
+    console.log(key,":- ",value); 
+}
+
+
+
+
+
+
+
