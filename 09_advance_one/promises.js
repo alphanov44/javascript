2e3be7ea642145df32632e64c5388bd63f56c9c7
alphanov44 <159747338+alphanov44@users.noreply.github.com>
2024-09-17 15:07:16 +0000

    .

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -9,3 +9,81 @@ const promiseOne=new Promise(function(resolve,reject){
 promiseOne.then(function(){
     console.log("Promise consumed");
 })
+
+//-----------------------------------------------------------------
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log("Async 2 ");
+        resolve();
+    },1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+//---------------------------------------------------------------------------------------------------------------
+
+const promiseThree=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username:"chai",  email:"chai@example"})
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+//---------------------------------------------------------------------------------------------------
+
+const promisefour= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false
+        if(!error)
+        {
+            resolve({username: "Aditi", password: "123"})
+        }
+        else{
+            reject('ERROR: something is wrong')
+        }
+    },1000)
+})
+
+promisefour
+.then((user)=>{
+    console.log(user);
+    return user.username;
+})
+.then((username)=>{
+    console.log(username);
+})
+.catch(function(error){
+    console.log(error);
+})
+.finally(()=>console.log("The Promise is either resolved or rejected"));
+
+//-------------------------------------------------------------------------------------------------------------------
+
+const promiseFive= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true
+        if(!error)
+        {
+            resolve({username: "Aditi", password: "123"})
+        }
+        else{
+            reject('ERROR: something is wrong')
+        }
+    },1000)
+})
+
+async function consumePromiseFive(){
+    try{
+        const response = await promiseFive
+        console.log(response);
+    } catch(error){
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+
+
